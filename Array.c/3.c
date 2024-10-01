aa6e5dc19a96ebbc21cad0c8078b571b46c0c58b
bishal <bishal@bishals-MacBook-Air.local>
2024-10-01 22:02:58 +0600

    array

---
+++ Array.c/3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int a [3],i,sum = 0;
+
+    for(i = 0 ; i < 3 ; i++){
+        scanf("%d", &a[i]);
+        
+         }
+
+    for(i = 0 ; i < 3 ; i++){
+        sum = sum + a[i];
+        
+         }
+
+         printf("the value of sum : %d\n",sum);
+
+
+
+
+
+
+
+
+    return 0;
+}
